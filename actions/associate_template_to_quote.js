644ed9855b0878b9b3abfa5eeb81a676bb49b051
Teun Rutten <teun.creative@gmail.com>
2022-11-29 21:10:19 +0100

    Add quote associaton action

---
+++ actions/associate_template_to_quote.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+exports.main = async (event, callback) => {
+  const quoteID = event.inputFields['quoteID'];
+
+  // SET THIS TO THE TEMPLATE ID OF YOUR CUSTOM QUOTE TEMPLATE
+  // The ID can be retrieved from the URL when you're editing you custom quote template
+  const templateID = undefined;
+
+  await axios({
+    method: 'put',
+    url: `https://api.hubapi.com/crm/v3/objects/quote/${quoteID}/associations/quote_template/${templateID}/quote_to_quote_template`,
+    headers: { 'Authorization': `Bearer ${process.env.secretName}`, 'Content-Type': 'application/json' },
+  }).then((response) => {
+    console.log(`Succesfully associated custom quote template ${templateID} to quote ${quoteID}`)
+  }).catch((error) => {
+    console.log(error.response.data)
+    throw new Error(error.response.data)
+  })
+}
+
